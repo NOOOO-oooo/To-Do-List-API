08792e46e85f2b243c31c6af49b3b9185d00b8f1
Abdalrahman Basim <86668061+NOOOO-oooo@users.noreply.github.com>
2025-01-22 17:28:45 +0300

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,13 +3,23 @@ const user = require("../controllers/userController");
 const task = require("../controllers/taskController");
 const { upload } = require("../taskPicMulter");
 
-router.post("/create", upload.single("picture"), task.createTask);
-router.put("/edit/desc/:task_id", task.editTaskDescription);
+router.post(
+   "/create",
+   upload.single("picture"),
+   user.ensureAuth,
+   task.createTask
+);
+router.put("/edit/desc/:task_id", user.ensureAuth, task.editTaskDescription);
 
-router.put("/edit/pic/:task_id", upload.single("picture"), task.editPic);
-router.put("/edit/stat/:task_id", task.markAsDone);
-router.put("/delete/pic/:task_id", task.deletePic);
+router.put(
+   "/edit/pic/:task_id",
+   user.ensureAuth,
+   upload.single("picture"),
+   task.editPic
+);
+router.put("/edit/stat/:task_id", user.ensureAuth, task.markAsDone);
+router.put("/delete/pic/:task_id", user.ensureAuth, task.deletePic);
 
-router.delete("/delete/:task_id", task.deleteTask);
+router.delete("/delete/:task_id", user.ensureAuth, task.deleteTask);
 
 module.exports = router;
